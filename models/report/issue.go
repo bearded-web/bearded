51bac2c1fa52b1d6a3ffdbc81923ba5622d85df2
Vyacheslav Bakhmutov <m0sth8@gmail.com>
2015-02-26 11:42:36 -0300

    Add HttpTransactions to issue report

--- models/report/issue.go
+++ models/report/issue.go
@@ -1,5 +1,7 @@
 package report
 
+import "net/http"
+
 type Url struct {
 	Url string `json:"url"`
 }
@@ -9,10 +11,29 @@ type Extra struct {
 	Title string `json:"title"`
 }
 
+type HttpBody struct {
+	ContentEncoding string `json:"contentEncoding"`
+	Content         []byte `json:"content"`
+}
+
+type HttpEntity struct {
+	Status string      `json:"status"`
+	Header http.Header `json:"header"`
+	Body   *HttpBody   `json:"body,omitempty"`
+}
+
+type HttpTransaction struct {
+	Id       int         `json:"id,omitempty"`
+	Method   string      `json:"method"`
+	Request  *HttpEntity `json:"request,omitempty"`
+	Response *HttpEntity `json:"response,omitempty"`
+}
+
 type Issue struct {
-	Severity Severity `json:"severity"`
-	Summary  string   `json:"summary"`
-	Desc     string   `json:"desc"`
-	Urls     []*Url   `json:"urls,omitempty" description:"where this issue is happened"`
-	Extras   []*Extra `json:"extras,omitempty" bson:"extras" description:"information about vulnerability"`
+	Severity         Severity           `json:"severity"`
+	Summary          string             `json:"summary"`
+	Desc             string             `json:"desc,omitempty"`
+	Urls             []*Url             `json:"urls,omitempty" description:"where this issue is happened"`
+	Extras           []*Extra           `json:"extras,omitempty" bson:"extras" description:"information about vulnerability"`
+	HttpTransactions []*HttpTransaction `json:"httpTransactions,omitempty" bson:"httpTransactions"`
 }
